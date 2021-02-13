5594537170d1ed7585d1e792c35f500e25da2c24
Regis Frey <RegisFrey@Darkfirestudios.com>
2021-02-13 14:27:26 -0800

    Trying something for github pages

--- generate-site.js
+++ generate-site.js
@@ -1,10 +1,10 @@
 const marked = require('marked'); // eslint-disable-line @typescript-eslint/no-var-requires
 const fs = require('fs'); // eslint-disable-line @typescript-eslint/no-var-requires
 
-const contentMarkdown = fs.readFileSync('./docs/index.md', 'utf-8');
+const contentMarkdown = fs.readFileSync('./index.md', 'utf-8');
 const contentRendered = marked(contentMarkdown);
 
-const themeHtml = fs.readFileSync('./docs/theme.html', 'utf-8');
+const themeHtml = fs.readFileSync('./theme.html', 'utf-8');
 const finalHtml = themeHtml.replace('<MarkdownContent/>', contentRendered)
 
-fs.writeFileSync('./docs/index.html', finalHtml);
+fs.writeFileSync('./index.html', finalHtml);
