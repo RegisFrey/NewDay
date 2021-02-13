b6d1fa76f7619a553736b78da995f900fab10fb1
Regis Frey <RegisFrey@Darkfirestudios.com>
2021-02-13 14:11:09 -0800

    Cleanup unused icon in calendar TODAY heading

--- src/components/Columns.vue
+++ src/components/Columns.vue
@@ -7,24 +7,6 @@
       <h2 class="splash-pad__section-head">
         <a href="https://calendar.google.com">
         Today
-        <!--
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          style="stroke: var(--color-text-subtle)"
-          fill="none"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          >
-            <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-            <line x1="16" y1="2" x2="16" y2="6"></line>
-            <line x1="8" y1="2" x2="8" y2="6"></line>
-            <line x1="3" y1="10" x2="21" y2="10"></line>
-        </svg>
-        <!-/- Feather - https://github.com/feathericons/feather - Calendar - MIT License -/->
-        -->
         </a>
       </h2>
       <Suspense><Calendar /></Suspense>
